1718c7ff093a794cf973b21db33c441e33c3eff0
Aniwange Tertese Amos <terteseamos@gmail.com>
2025-07-17 13:05:35 +0100

    feat: initial commit

---
+++ src/main/java/com/ecews/outreachquestionnaire/OutreachQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.ecews.outreachquestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutreachQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutreachQuestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecews/outreachquestionnaire/OutreachQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecews.outreachquestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutreachQuestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
